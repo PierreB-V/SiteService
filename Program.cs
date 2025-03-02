8dad4abdf54d2c3a95992b8f0803398a9d7aa045
Pierre <pierre.bouffard-vercelli@epc-groupe.com>
2025-03-02 10:38:14 +0100

    Ajout de la télémétrie et journalisation avec OpenTelemetry
    - Ajout des directives `using` pour les bibliothèques de journalisation et télémétrie.
    - Initialisation d'une fabrique de logger et création d'un logger nommé "Program".
    - Configuration d'OpenTelemetry pour les traces et journaux avec exportateur de console.
    - Ajout de journaux d'information pour les points de terminaison `/weatherforecast`, `/site/{id}` et `/site`.
    - Modification du paramètre de la route `/site/{id}` de `id` à `siteId` et ajout d'un délai simulé de 2000 ms.
    - Ajout d'un nouveau point de terminaison `/site` avec un délai simulé de 2000 ms.
    - Ajout de packages NuGet pour OpenTelemetry et Serilog dans `SiteService.csproj`.

--- Program.cs
+++ Program.cs
@@ -1,13 +1,51 @@
 using SiteService.Model;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry.Trace;
+using OpenTelemetry.Resources;
+using System.Diagnostics;
+using System.Reflection;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Logs;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
+using var loggerFactory = LoggerFactory.Create(builder =>
+{
+    builder.AddConsole();
+});
+
+
+var logger = loggerFactory.CreateLogger("Program");
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(resource => resource
+        .AddService("SiteService", "diogen", Assembly.GetExecutingAssembly().GetName().Version!.ToString())
+        .AddAttributes(new[]
+        {
+            new KeyValuePair<string, object>("service.environment", "debug")
+        }))
+    .WithTracing(tracing => tracing
+        .AddAspNetCoreInstrumentation()
+        .AddSource("SiteService")
+        .AddConsoleExporter()
+    )
+    //.WithMetrics(metrics =>metrics
+    //    .AddAspNetCoreInstrumentation()
+    //    .AddConsoleExporter()
+    //)
+    .WithLogging(logging=> logging
+        .AddConsoleExporter()
+    );
+
+
 var app = builder.Build();
+logger.LogDebug("Starting application");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -23,6 +61,7 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
+    logger.LogInformation("called weather");
     var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
@@ -36,18 +75,35 @@ app.MapGet("/weatherforecast", () =>
 .WithName("GetWeatherForecast")
 .WithOpenApi();
 
-app.MapGet("/site/{id}", (int id) =>
+app.MapGet("/site/{id}", (int siteId) =>
 {
+    logger.LogInformation($"calling site {siteId}", siteId);
+    Task.Delay(2000);
+
+
     return new Site
     {
-        Id = id,
-        Name = "Site " + id,
+        Id = siteId,
+        Name = "Site " + siteId,
         Latitude = (float)(Random.Shared.NextDouble() * 60),
         Longitude = (float)(Random.Shared.NextDouble() * 360 - 180)
     };
 })
 .WithName("GetSite")
 .WithOpenApi();
+
+app.MapGet("/site", () =>
+{
+    logger.LogInformation("calling sites");
+    Task.Delay(2000);
+    return Enumerable.Range(1, 5).Select(i => new Site
+    {
+        Id = i,
+        Name = "Site " + i,
+        Latitude = (float)(Random.Shared.NextDouble() * 60),
+        Longitude = (float)(Random.Shared.NextDouble() * 360 - 180)
+    });
+});
 
 
 app.Run();
